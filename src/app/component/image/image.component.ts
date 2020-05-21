9b223761d4052bf74de5b971e6cd9dc222b2fee8
Shreya Goyal <shreyagoyal531990@gmail.com>
2020-05-21 18:03:32 +0530

    same div updation

--- src/app/component/image/image.component.ts
+++ src/app/component/image/image.component.ts
@@ -10,7 +10,8 @@ import * as firebase from 'firebase';
 })
 export class ImageComponent implements OnInit {
   @Input() image_url: any;
-  // @Output() image_added = new EventEmitter();
+  @Input() index: any;
+  @Output() new_image_added = new EventEmitter();
   uploadPercent: any;
   downloadURL: any;
 
@@ -18,33 +19,38 @@ export class ImageComponent implements OnInit {
     private storage: AngularFireStorage,
     private afs: AngularFirestore
   ) { }
-  delete(item){
-    if(item){
-      var self=this;
-      const filePath = 'apputnixPhoto/'+item['id'];
-  
-      self.storage.ref(filePath).delete().subscribe(async(data)=>{
-        try{
+  delete(item) {
+    if (item) {
+      var self = this;
+      const filePath = 'apputnixPhoto/' + item['id'];
+
+      self.storage.ref(filePath).delete().subscribe(async (data) => {
+        try {
           // var data={ image:image.filter(e=>e.item_id!=item_id) }
-          console.log("Image deleted successfully",data)
+          console.log("Image deleted successfully", data)
           await self.afs.collection('images').doc('123')
-          .update({image:firebase.firestore.FieldValue.arrayRemove({
-            id:item.id,
-            image_path:item.image_path
-  
-          })})
+            .update({
+              image: firebase.firestore.FieldValue.arrayRemove({
+                id: item.id,
+                image_path: item.image_path
+
+              })
+            })
           console.log("Item deleted successfully")
         }
-  
-        
-        catch(error){
-          console.log("Error to delete",error)
+
+
+        catch (error) {
+          console.log("Error to delete", error)
         }
-    
-    })
+
+      })
     }
-  
-}
+
+  }
+  ngAfterViewInit() {
+    console.log("***index*****", this.index)
+  }
   ngOnInit() { }
   gotImage(event) {
     var self = this;
@@ -52,48 +58,94 @@ export class ImageComponent implements OnInit {
     const id = Math.random().toString(36).substring(2);
 
     const file = event.target["files"][0];
-    const filePath = 'apputnixPhoto/'+id;
+    const filePath = 'apputnixPhoto/' + id;
     const ref = this.storage.ref(filePath);
     const task = ref.put(file)
+    task.then(async (data) => {
+      ref.getDownloadURL().subscribe(async (data) => {
+        console.log("self.downloadURL", data)
 
-    // observe percentage changes
-    task.percentageChanges().subscribe((data) => {
-      this.uploadPercent = data
-      console.log("    this.uploadPercent", this.uploadPercent)
-      if (self.uploadPercent == 100) {
-        ref.getDownloadURL().subscribe(async (data) => {
-          console.log("self.downloadURL", data)
+        self.downloadURL = data
+        console.log("self.image_url", self.image_url)
+        if (!self.image_url) {
+          console.log("not of self.image_url")
+          try {
+            await self.afs.collection('images').doc('123')
+              .set({
+                image: firebase.firestore.FieldValue.arrayUnion({
+                  id: id,
+                  image_path: self.downloadURL
+                })
+              },
+                { merge: true })
+          }
+          catch (error) {
+            console.log("Error while uplaoding image", error)
+          }
+        }
+        else {
+          console.log("presence of image_url")
+          // const booking = { some: "data" };
+          var db = firebase.firestore()
+          const imageRef = db.collection("images").doc('123');
 
-          self.downloadURL = data
-          await self.afs.collection('images').doc('123')
-            .set({ image: firebase.firestore.FieldValue.arrayUnion({id:id,image_path:self.downloadURL}) }, { merge: true })
-            // submitted_data?console.log("submitted successfully"):console.log("not submitted successfully")
-          // self.image_added.emit(self.downloadURL)
+          db.runTransaction((transaction: any) => {
+            // This code may get re-run multiple times if there are conflicts.
+            return transaction.get(imageRef).then(doc => {
+              if (!doc.data().image) {
+                console.log("Image object is not present")
+                transaction.set({
+                  image: firebase.firestore.FieldValue.arrayUnion({
+                    id: id,
+                    image_path: self.downloadURL
+
+                  })
+                })
+              } else {
+                console.log("entered iamge presence")
+                var images: any;
+                images = doc.data().image;
+                var new_array = images.map((e) => {
+                  console.log("enterd checking field")
+                  if (e.id == self.image_url.id) {
+                    return {
+                      id: id,
+                      image_path: self.downloadURL
+                    }
+                  }
+                })
+                console.log("Updated array about to inserted", [...new_array])
+                transaction.update(imageRef, { image: [...new_array] });
+              }
+            });
+          }).then(function () {
+            console.log("Transaction successfully committed!");
+          }).catch(function (error) {
+            console.log("Transaction failed: ", error);
+          });
+          // await self.afs.collection('images').doc('123').update({
+          //   image: firebase.firestore.FieldValue.arrayUnion({
+          //     id: id,
+          //     image_path: self.downloadURL
+
+          //   })
+          // })
+
+        }
+
+        // submitted_data?console.log("submitted successfully"):console.log("not submitted successfully")
+        // self.image_added.emit(self.downloadURL)
+
+      })
+        , ((error) => {
+          console.log("Error while downloading image", error)
 
         })
-          , ((error) => {
-            console.log("Error while downloading image", error)
-          })
-      }
-
-    }, (error) => {
-      console.log("Error on uploading image", error)
     })
-    // ref.getDownloadURL().subscribe((data)=>{
-    //   console.log("self.downloadURL",data)
+      .catch((error) => {
+        console.log("Error of task is", error)
+      })
 
-    //   self.downloadURL =data
-    // })
-    // get notified when the download URL is available
-    //     task.snapshotChanges().pipe(
-    //         finalize(() => {
-    //           self.downloadURL = ref.getDownloadURL()
-    //           console.log("self.downloadURL",self.downloadURL)
-
-    //         }
-    //          )
-    //      )
-    //     .subscribe((data)=>{
     //       console.log("data after  finalizing the build",data)
     //       console.log("data after url is downloaded",self.downloadURL)
     // self.afs.collection('images').doc('123').set({image:firebase.firestore.FieldValue.arrayUnion(self.downloadURL)},{merge:true})
